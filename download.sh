b9b12637f7d2db741f13ba85916dc0647399acbe
Guillermo José Gutiérrez Caracuel <guilleguca@gmail.com>
2026-04-14 13:09:49 +0200

    Remove API key from download.sh
    Removed hardcoded API key from download script.

--- download.sh
+++ download.sh
@@ -6,5 +6,4 @@ for lineage in B.1.1.7 B.1.351 P.1 B.1.617.2 B.1.1.529; do
         --lineage $lineage \
         --complete-only \
         --filename data/raw/${lineage}.zip
-	--api-key 5d61c24cc13750711a985fc4a7c5eb483408
 done
